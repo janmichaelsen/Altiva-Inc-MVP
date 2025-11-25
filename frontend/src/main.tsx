4418703c5bc425fda8df1a1a19ada0154d35b73d
Felipe Alvarez <felipealvarez.mer@gmail.com>
2025-11-25 11:38:10 -0300

    Incorporacion de google

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css' // ¡CRÍTICO! Esto carga Tailwind
+import App from './App.tsx'
+import './index.css'
+
+//proveedor de Google
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+
+const GOOGLE_CLIENT_ID = "55062076132-rmsbo51r1oht9ls3tt5s847tqfmc1es2.apps.googleusercontent.com" ; 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {/* Envolvemos toda la app con el proveedor de Google */}
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>,
 )
